73eabb724830ac46937205cb5e9603bed38e2bc1
Manav Kaushal <manavkaushal756@gmail.com>
2023-01-15 14:15:37 +0530

    updated folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "react-before-after-slider-component/dist/build.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
