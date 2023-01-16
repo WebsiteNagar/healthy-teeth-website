7556b36fa174caa4e8e44a15a42c0a3deb370537
Manav Kaushal <manavkaushal756@gmail.com>
2023-01-16 18:50:48 +0530

    added src folder in tailwind config

---
+++ src/components/BroadcastStrip/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const BroadcastStrip = (props: Props) => {
+  return <div className="w-full h-10 bg-primary">BroadcastStrip</div>;
+};
+
+export default BroadcastStrip;
