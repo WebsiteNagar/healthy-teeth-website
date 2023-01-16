1ac36d8999aa38692dbfdde6ac63612ba5084473
Manav Kaushal <manavkaushal756@gmail.com>
2023-01-16 18:47:37 +0530

    updated theme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00a3c8",
+        headings: "#004861",
+        "primary-hover": "#0056b3",
+      },
+    },
   },
   plugins: [],
-}
+};
